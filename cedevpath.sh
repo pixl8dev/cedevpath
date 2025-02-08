e5e1bd6670f8233c82cdfa3933599d087cac6d25
Pixl8 <68357832+pixl8dev@users.noreply.github.com>
2025-02-08 16:09:46 -0500

    Update cedevpath.sh

--- cedevpath.sh
+++ cedevpath.sh
@@ -1,1 +1,2 @@
-echo "this works :)"
+clear
+sudo apt install make -y && sudo apt install git -y && rm -rf ~/tidev && git clone https://github.com/pixl8dev/cedevbin.git && echo 'export PATH="$HOME/cedevbin/bin:$PATH"' >> ~/.bashrc && source ~/.bashrc
